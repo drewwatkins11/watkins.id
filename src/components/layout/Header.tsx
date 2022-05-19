35454c5bff13548ac89335572cd3d31cc5c6b0c7
Drew Watkins <drew@watkins.id>
2022-05-19 10:29:37 -0400

    Add social links and BOLO videos

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,7 @@ import Logomark from "../../assets/img/dw_logomark.svg";
 const Header: React.FC = () => {
   return (
     <Flex w="full" justify="space-between" direction="row">
-      <img src={Logomark} />
+      {/* <img src={Logomark} /> */}
     </Flex>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,61 @@
 import React from "react";
-import { Flex, Heading, Stack, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Link,
+  ListItem,
+  Stack,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
 import Layout from "../layouts/Layout";
 
 export default function Home() {
   return (
     <Layout>
-      <Flex direction="column">
+      <Stack direction="column">
         <Text>About</Text>
         <Heading as="h1" size="h1">
           Drew Watkins
         </Heading>
         <Text>Product leader, developer, technologist, and tinkerer.</Text>
-      </Flex>
+        <Stack>
+          <Heading as="h2" size="h4">
+            <strong>Find me on:</strong>
+          </Heading>
+          <UnorderedList>
+            <ListItem>
+              <Link href="https://medium.com/@drewwatkins11">Medium</Link>
+            </ListItem>
+            <ListItem>
+              <Link href="https://linkedin.com/in/drewwatkins11">LinkedIn</Link>
+            </ListItem>
+          </UnorderedList>
+        </Stack>
+        <Stack>
+          <Heading as="h2" size="h4">
+            <strong>Listen to me</strong>
+          </Heading>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/hcD7StumxN0"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/mgW9GbGaZVI"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </Stack>
+      </Stack>
     </Layout>
   );
 }
